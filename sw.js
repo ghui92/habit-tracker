83dd42097f10ba1dfa8db86b5e00a7d4f6639713
ghui92 <18410356769@163.com>
2026-06-10 10:44:44 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE = 'habitpwa-v1';
+var URLS = ['index.html', 'apple-touch-icon.png', 'favicon.png', 'icon.svg'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache){
+      return cache.addAll(URLS);
+    }).then(function(){ return self.skipWaiting() })
+  );
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){
+        if(k !== CACHE) return caches.delete(k);
+      }));
+    }).then(function(){ return self.clients.claim() })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      return cached || fetch(e.request).then(function(res){
+        return caches.open(CACHE).then(function(cache){
+          if(e.request.url.indexOf('http')===0) cache.put(e.request, res.clone());
+          return res;
+        });
+      });
+    }).catch(function(){ return caches.match('index.html') })
+  );
+});
